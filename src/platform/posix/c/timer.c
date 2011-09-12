ad561cf9ee806e0a385c7a3e0096ec6cd49ce938
omaye <omaye@6fef45d4-bfbb-4794-bc12-f429be1069c5>
2011-09-12 08:18:40 +0000

    Definitions and implementation for the fewos platform

--- src/platform/posix/c/timer.c
+++ src/platform/posix/c/timer.c
@@ -19,16 +19,15 @@
  * along with Darjeeling.  If not, see <http://www.gnu.org/licenses/>.
  */
  
-#include <time.h>
-
 #include "types.h"
+#include "clock.h"
 
 void dj_timer_init()
 {
-
+	clockInit();
 }
 
 uint32_t dj_timer_getTimeMillis()
 {
-	return clock() / (CLOCKS_PER_SEC / 1000);
+	return clockGetMillis();
 }
