cd4d7960d3c7ef0e8a5ce6fc492d94fbc56f87b6
omaye <omaye@6fef45d4-bfbb-4794-bc12-f429be1069c5>
2011-09-13 12:02:53 +0000

    Implemented LED functions

--- src/lib/darjeeling2/c/fewos/javax_darjeeling_actuators_Leds.c
+++ src/lib/darjeeling2/c/fewos/javax_darjeeling_actuators_Leds.c
@@ -24,8 +24,74 @@
 #include "array.h"
 #include "execution.h"
 #include "jlib_base.h"
+#include "gpio.h"
 
+#if (BOARD==MATRIXONE)
+#define NUM_LEDS 4
+static int portNum[] = {4, 4, 4, 4, 5, 5};
+static uint8_t bitNum[] = {0x08, 0x10, 0x20, 0x40, 0x10, 0x20};
+#define LED_ENABLE_IDX		4
+#define LED_GROUP_SEL_IDX	5
+
+#elif (BOARD==EXPERIMENTER)
+#define NUM_LEDS 2
+static int portNum[] = {1, 1};
+static uint8_t bitNum[] = {0x01, 0x02};
+
+#elif (BOARD==FEUERWHERE)
+#define NUM_LEDS 8
+static int portNum[] = {8, 8, 8, 8, 8, 8, 8, 8};
+static uint8_t bitNum[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
+
+#else
 #define NUM_LEDS 0
+static int portNum[] = {};
+static uint8_t bitNum[] = {};
+#endif
+
+void initIO( int nr ) {
+	int err;
+	uint8_t data;
+	// Set port select = I/O
+	data = gpioRead( portNum[nr], gpioPortSelection );
+	err = gpioWrite( portNum[nr], gpioPortSelection, data & ~bitNum[nr] );
+	// Set direction = output.
+	data = gpioRead( portNum[nr], gpioDirection );
+	err = gpioWrite( portNum[nr], gpioDirection, data | bitNum[nr] );
+	// Set drive strength = full.
+	data = gpioRead( portNum[nr], gpioDriveStrength );
+	err = gpioWrite( portNum[nr], gpioDriveStrength, data | bitNum[nr] );
+}
+
+void setIO( int nr, int on ) {
+	int err;
+	uint8_t data;
+	// Set LED on/off
+	data = gpioRead( portNum[nr], gpioOutput );
+	if( on ) {
+		err = gpioWrite( portNum[nr], gpioOutput, data | bitNum[nr] );
+	} else {
+		err = gpioWrite( portNum[nr], gpioOutput, data & ~bitNum[nr] );
+	}
+}
+
+void initLed() {
+	int idx=0;
+
+#if (BOARD==MATRIXONE)
+	initIO(LED_ENABLE_IDX);
+	initIO(LED_GROUP_SEL_IDX);
+	setIO(LED_ENABLE_IDX, 1);		// LED enable
+	setIO(LED_GROUP_SEL_IDX, 1);	// Group A = LED 1,3,5,7
+#endif
+
+	for( idx=0; idx < NUM_LEDS; idx++ ) {
+		initIO(idx);
+	}
+	for( idx=0; idx < NUM_LEDS; idx++ ) {
+		setIO(idx, 0);
+	}
+}
 
 //short javax.darjeeling.actuators.Leds.getNrLeds()
 void javax_darjeeling_actuators_Leds_short_getNrLeds()
@@ -36,9 +102,10 @@ void javax_darjeeling_actuators_Leds_short_getNrLeds()
 // void javax.darjeeling.actuators.Leds.set(short, boolean)
 void javax_darjeeling_actuators_Leds_void_set_short_boolean()
 {
-	/*uint16_t on =*/ dj_exec_stackPopShort();
-
-	// Discard the led index argument
-	/*uint16_t nr =*/ dj_exec_stackPopShort();
-
+	uint16_t on = dj_exec_stackPopShort();
+	// Get the led index argument
+	uint16_t nr = dj_exec_stackPopShort();
+	if( nr < NUM_LEDS ) {
+		setIO(nr, on);
+	}
 }
