080a65f9734b0a785dfc44c70c1a780bdc7e556f
omaye <omaye@6fef45d4-bfbb-4794-bc12-f429be1069c5>
2012-01-13 15:33:47 +0000

    Fixed Benchmark and MemoryWatcher to deliver more expressive results.

--- src/app/benchmark/java/com/ihp/Benchmark.java
+++ src/app/benchmark/java/com/ihp/Benchmark.java
@@ -15,82 +15,41 @@ public class Benchmark {
 	 */
 	public static void main(String[] args) {
 		test(new HelloWorld());
-//		test(new BubbleSort());
-//		test(new QuickSort());
-//		test(new TowersOfHanoi((short)12));
-//
-//		test(new RSA());
+		test(new BubbleSort());
+		test(new QuickSort());
+		test(new TowersOfHanoi((short)12));
+		test(new RSA());
 		
 		
 	}
 	
-//	public static void synchronousGC() {
-//		Runtime.getRuntime().gc();
-//		long free1 = 0;
-//		long free2 = 0;
-//		do {
-//			free1 = free2;
-//			free2 = Runtime.getRuntime().freeMemory();
-//			try {
-//				Thread.sleep(1000);
-//			} catch (Exception e) {
-//			}
-//		} while (free2 != free1);
-//	}
-
-//	private static void printMemory(Runtime r) {
-//		long free = r.freeMemory();
-//		String freeS = Long.toString(free);
-//		Darjeeling.print(freeS);
-//		Darjeeling.print(" - ");
-//		long total = r.totalMemory();
-//		String totalS = Long.toString(total);
-//		Darjeeling.print(totalS);
-//		Darjeeling.print("\n");
-//	}
-
 	private static void test(TestImplementation timpl) {
-		Runtime runtime = Runtime.getRuntime();
+		MemoryWatcher memoryWatcher = startMemoryWatcher();
 		for (int i = 0; i < 6; i++) {
 			int j = 1 << i;
-			System.out.println( "Running " + j + " times " + timpl.getName() );
-			long freebefore = runtime.freeMemory();
+			memoryWatcher.explicitGC();
+			memoryWatcher.reset();
+
 			long start = System.currentTimeMillis();
 			timpl.runTest(j);
 			long end = System.currentTimeMillis();
-			long freeafter = Runtime.getRuntime().freeMemory();
-			System.out.println("Time elapsed: " + (end-start) + " ms.");
-			System.out.println("Memory used : " + (freebefore-freeafter)+ " bytes." );
-			
-//			memoryWatcher.cancel();
-//			Darjeeling.println("gc()");
-//			long freeDuringMin = 0;//memoryWatcher.getMinFree();
-//			long freeDuringMax = memoryWatcher.getMaxFree();
-//			runtime.gc();
-//			Darjeeling.println(timpl.getName());
-//			Darjeeling.println(Long.toString(freebefore));
-//			long duration = end - start;
-//			Darjeeling.println(Long.toString(duration));
-//			Darjeeling.println(timpl.getName() + "(" + j + " x)" + " took "
-//					+ (end - start) + "ms");
-//			Darjeeling.println("memory use:"+/*(freebefore-freeDuringMax)+" - "* /+(freebefore-freeDuringMin));
-			
+			memoryWatcher.measure();
+			memoryWatcher.explicitGC();
+			memoryWatcher.measure();
+			long min = memoryWatcher.getMinUsed();
+			long max = memoryWatcher.getMaxUsed();
+
+			System.out.println(timpl.getName() + "(" + j + "x)" + " took " +
+								(end - start) + "ms. Memory use: " +
+								min + " - " + max );
 		}
+		memoryWatcher.cancel();
 	}
 	
-//	private static MemoryWatcher startMemoryWatcher() {
-//		MemoryWatcher mw = new MemoryWatcher();
-//		mw.start();
-//		return mw;
-//	}
+	private static MemoryWatcher startMemoryWatcher() {
+		MemoryWatcher mw = new MemoryWatcher();
+		mw.start();
+		return mw;
+	}
 
-//	private static void dump(short[] testfield) {
-//		for(int i = 0; i < testfield.length;) {
-//			String dumpline = "";
-//			for (int j = 0; j < 16 && i < testfield.length; j++, i++) {
-//				dumpline += testfield[i] + " ";
-//			}
-//			Darjeeling.println(dumpline);
-//		}
-//	}
 }
--- src/app/benchmark/java/com/ihp/MemoryWatcher.java
+++ src/app/benchmark/java/com/ihp/MemoryWatcher.java
@@ -11,29 +11,44 @@ public class MemoryWatcher extends Thread {
 
 	private boolean stopped = false;
 	private Runtime myRuntime;
-	private long maxFree = 0;
-	private long minFree = -1;
+	private long maxFree;
+	private long minFree;
+	private long base;
+	private Object lock;
 
 	public MemoryWatcher() {
 		myRuntime = Runtime.getRuntime();
+		lock = new Object();
+		reset();
 	}
 	
 	public void run() {
 		while (!stopped ) {
-			try {
-				Thread.sleep(10);
-			} catch (Exception e) {
-			}long free = myRuntime.freeMemory();
-			maxFree = Math.max(maxFree, free);
-			if (minFree < 0) {
-				minFree = free;
-			} else {
-				minFree = Math.min(minFree , free);
-			}
+			measure();
 			try {
 				Thread.sleep(40);
 			} catch (Exception e) {
 			}
+		}
+	}
+	
+	public void explicitGC() {
+		myRuntime.gc();
+	}
+	
+	public void measure() {
+		synchronized(lock) {
+			long free = myRuntime.freeMemory();
+			maxFree = Math.max(maxFree, free);
+			minFree = Math.min(minFree , free);
+		}
+	}
+	
+	public void reset() {
+		synchronized(lock) {
+			base = myRuntime.freeMemory();
+			maxFree = base;
+			minFree = base;
 		}
 	}
 
@@ -43,6 +58,14 @@ public class MemoryWatcher extends Thread {
 
 	public long getMaxFree() {
 		return maxFree;
+	}
+	
+	public long getMinUsed() {
+		return (base-maxFree);
+	}
+	
+	public long getMaxUsed() {
+		return (base-minFree);
 	}
 
 	public void cancel() {
